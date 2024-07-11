fe9a3e6688257d50c40b5da8fb4b95e381899d4b
DashD007 <100300164+DashD007@users.noreply.github.com>
2024-07-12 00:32:34 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,23 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx"
 import { Toaster } from "react-hot-toast";
-import {useEffect} from "react"
-import { useDispatch } from "react-redux";
+// import {useEffect} from "react"
+// import { useDispatch } from "react-redux";
 import { setAuthUser } from "./redux/userSlice.js";
 import {setallDoubts, setdoubtTree} from "./redux/doubtSlice.js"
 import {setResearchPapers, setRelatedSearches} from "./redux/researchSlice.js"
 function App() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    return () => {
+  // const dispatch = useDispatch();
+  // useEffect(() => {
+  //   return () => {
       
-      dispatch(setAuthUser(null));
-      dispatch(setallDoubts([]));
-      dispatch(setdoubtTree([]));
-      dispatch(setResearchPapers([]));
-      dispatch(setRelatedSearches([]));
-    }
-  }, [dispatch])
+  //     dispatch(setAuthUser(null));
+  //     dispatch(setallDoubts([]));
+  //     dispatch(setdoubtTree([]));
+  //     dispatch(setResearchPapers([]));
+  //     dispatch(setRelatedSearches([]));
+  //   }
+  // }, [dispatch])
 
   return (
     <div >
