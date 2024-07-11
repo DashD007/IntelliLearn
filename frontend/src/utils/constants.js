bbcb6cb15128d195eef7ce135cf1a4a6f46ac894
DashD007 <Damodarsh2002@gmail.com>
2024-07-11 14:30:40 +0530

    added backend URI

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -3,4 +3,4 @@ export const userAvatar = "https://e7.pngegg.com/pngimages/340/946/png-clipart-a
 export const blogPlaceholder = "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png";
 
 
-export const backendURI = "http://localhost:8080/api/v1";
+export const backendURI = "https://intellilearn-backend.vercel.app/api/v1";
