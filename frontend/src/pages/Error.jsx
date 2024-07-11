a9749fc5cb89caecc0eb77ce8773bc51ba407c8a
DashD007 <Damodarsh2002@gmail.com>
2024-07-11 13:31:58 +0530

    "error page changed"

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import image from "../assests/images/error.svg"
 const Error = () => {
   return (
-    <div>Error</div>
+    <div className='w-full flex items-center justify-center'>
+      <img src={image} alt="error-image" className='object-cover'/>
+    </div>
   )
 }
 
